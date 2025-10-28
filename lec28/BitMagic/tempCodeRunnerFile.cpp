38ea9c1fca74dfac4751f53ae80181a5a6227dd7
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-28 15:30:25 +0530

    CODE

--- lec28/BitMagic/tempCodeRunnerFile.cpp
+++ lec28/BitMagic/tempCodeRunnerFile.cpp
@@ -1,11 +1,16 @@
 #include<stdio.h>
 int main(){
-    int n=5;
+    int n;
+    printf("enter any no.:\n");
+    scanf("%d",&n);
+    printf("\n");
+    printf("you entered %d",n);
+    printf("\n");
     if(n&1)
     {
-        printf("ODD");
+        printf(" num. is ODD");
     }
     else{
-        printf("EVEN");
+        printf("num. is EVEN");
     }
 }
