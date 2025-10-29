012afa059966315c183e631177f89a66c1543626
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-29 15:28:42 +0530

    CODE

--- lec28/BitMagic/MissingNum.cpp
+++ lec28/BitMagic/MissingNum.cpp
@@ -6,12 +6,12 @@ int main()
     int y=0;
      for(int i=0;i<=5;i++)
     {
-         x=x^i;
+        int x=x^i;
     }
     //int arr[50]={1,2,4,5};
     for(int i=0;i<4;i++)
     {
-       y=y^arr[i];
+       int y=y^arr[i];
     }
     int a=x^y;
     printf("missing number=%d",a);
