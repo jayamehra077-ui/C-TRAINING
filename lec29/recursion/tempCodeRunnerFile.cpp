edaeab7ffa221f9f21d68311a4568f7943869e62
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-30 15:17:53 +0530

    CODE

--- lec29/recursion/tempCodeRunnerFile.cpp
+++ lec29/recursion/tempCodeRunnerFile.cpp
@@ -12,7 +12,10 @@ int main()
 {
     sum(5);
 }*/
-#include<stdio.h>
+
+
+
+/*#include<stdio.h>
 void sum(int x)
 {
     if(x==0)
@@ -26,4 +29,20 @@ void sum(int x)
 int main()
 {
     sum(5);
+}*/
+
+
+#include<stdio.h>
+int sum(int x)
+{
+    if(x==0)
+    {
+        return 0;
+    }
+    return x+sum(x-1);
+}
+int main()
+{
+    int data=sum(5);
+    printf("%d",data);
 }
